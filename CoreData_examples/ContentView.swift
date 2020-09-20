41d532c20c188d5c888c18e0f884d1d0f4ac7634
Max <maxim.macari.dam@gmail.com>
2020-09-20 23:58:38 +0200

    Initial Commit

---
+++ CoreData_examples/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoreData_examples
+//
+//  Created by Maxim Macari on 20/09/2020.
+//  Copyright © 2020 Maxim Macari. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
